3a6b0e24d7b3e1702071e7db15cc8bf24dd03b17
Petr Pošvic <petr@posvic.cz>
2018-01-13 13:35:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Notify Update
+Notify me via e-mail if any update is available
---
+++ notify-update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+MAIL_TO="petr@posvic.cz"
+MAIL_FROM="notification@posvic.cz"
+MAIL_SERVER="smtp.gmail.com:587"
+MAIL_USER="notification@posvic.cz"
+
+# Go to https://myaccount.google.com/lesssecureapps
+# Enable 2-phase authentication, select Application Passwords and add new one
+MAIL_PASS=""
+
+# Edit cron (crontab -e) in root account and insert this line (without #):
+# 0 2 * * * /path/to/this/script/notify-update.sh > /dev/null 2>&1
+
+if [ ! -f /usr/bin/sendemail ]; then
+  echo "Install sendemail!"
+  echo "  sudo apt install sendemail"
+  exit -1
+fi
+
+upd=`apt-get update`
+msg=`apt-get -s upgrade | grep -i "^inst"`
+
+if [ -z "$msg" ]; then
+  echo "No updates"
+  exit 1
+fi
+
+echo "Updates available, send notification to $MAIL_TO"
+echo "Updates available for $HOSTNAME" | /usr/bin/sendemail -u "Update $HOSTNAME" \
+  -f $MAIL_FROM -t $MAIL_TO \
+  -s $MAIL_SERVER -xu $MAIL_USER -xp $MAIL_PASS \
+  -o tls=yes > /dev/null 2>&1
